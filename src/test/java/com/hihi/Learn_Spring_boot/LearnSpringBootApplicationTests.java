fc9b103445db77596a5129381e78a83d4cf81448
hungnguyen-160104 <hungnguyen160104@gmail.com>
2025-02-18 23:06:20 +0700

    Hoc_spring_boot

---
+++ src/test/java/com/hihi/Learn_Spring_boot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+ package com.hihi.Learn_Spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
